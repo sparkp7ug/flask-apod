7123dd0efd3947231776bf1d833f8ff371cdb04d
Yogesh Singh <yogesh.singh.ams5@gmail.com>
2020-01-07 11:42:43 +0530

    date picker with home page

--- flask_nasa.py
+++ flask_nasa.py
@@ -6,16 +6,20 @@ import datetime
 
 app = Flask(__name__)
 
-@app.route("/apod/<string:date>/")
-def hello(date):
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/apod/<string:date>")
+def apod(date):
     if(datetime.datetime.today() > datetime.datetime.strptime(date,'%Y-%m-%d')):
         api_response = urllib3.PoolManager().request('GET','https://api.nasa.gov/planetary/apod?api_key=x7qyRICNLMzDbFt14kL4I3SUeBMWKpziBecV6u07&date='+date)
         result_json = json.loads(api_response.data.decode('utf-8'))
-        return render_template('test.html',imgsrc=result_json['url'],date=date,title=result_json['title'])
+        return render_template('apod.html',imgsrc=result_json['url'],date=date,title=result_json['title'])
     else:
         return '<html><body><h1>Error 404</h1><p>Given date is in future.</p></body></html>'
 
-@app.route("/download/<string:date>/")
+@app.route("/download/<string:date>")
 def download(date):
     if(datetime.datetime.today() > datetime.datetime.strptime(date,'%Y-%m-%d')):
         api_response = urllib3.PoolManager().request('GET','https://api.nasa.gov/planetary/apod?api_key=x7qyRICNLMzDbFt14kL4I3SUeBMWKpziBecV6u07&date='+date)
